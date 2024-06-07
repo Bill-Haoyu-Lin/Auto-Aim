5a4dc9a2d8da33e6451d757670d6ce54e758b15e
Bill <Onbilllin@gmail.com>
2024-06-06 22:49:32 -0400

    remove depth stream

--- CamDataCap.py
+++ CamDataCap.py
@@ -13,173 +13,196 @@ class depth_camera:
     calculationAlgorithm = dai.SpatialLocationCalculatorAlgorithm.AVERAGE
     coordinates_3d = [0,0,0]
     angles_default = [0,0,0]
+    angles_min =[0,0,0]
     colorfps = 60
     cur_angle = [0,0,0]
+    cam_started = False
+    depth_on = False
 
     def __init__(self):
+        
         self.frame = {"video": None, "disparity": None}
         self.pipeline = dai.Pipeline()
 
         #create pipeline components
-        monoLeft = self.pipeline.create(dai.node.MonoCamera)
-        monoRight = self.pipeline.create(dai.node.MonoCamera)
-        color = self.pipeline.create(dai.node.ColorCamera)
-        stereo = self.pipeline.create(dai.node.StereoDepth)
-        spatialLocationCalculator = self.pipeline.create(dai.node.SpatialLocationCalculator)
-        imu = self.pipeline.create(dai.node.IMU)
-        sync = self.pipeline.create(dai.node.Sync)
 
+        
+        
+        #color camera
+        color = self.pipeline.create(dai.node.ColorCamera)
         #Config color camera
         color.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
         color.setFps(self.colorfps)
+        color.setCamera("color")
+
+        manip = self.pipeline.createImageManip()
+
+
+        #imu
+        imu = self.pipeline.create(dai.node.IMU)
+
+
+
+        if self.depth_on:
+
+            monoLeft = self.pipeline.create(dai.node.MonoCamera)
+            monoRight = self.pipeline.create(dai.node.MonoCamera)
+
+            stereo = self.pipeline.create(dai.node.StereoDepth)
+            spatialLocationCalculator = self.pipeline.create(dai.node.SpatialLocationCalculator)
+
+            #Add for depth video sync
+            xoutDepth = self.pipeline.create(dai.node.XLinkOut)
+            xoutSpatialData = self.pipeline.create(dai.node.XLinkOut)
+            xinSpatialCalcConfig = self.pipeline.create(dai.node.XLinkIn)
+
+            xoutDepth.setStreamName("depth")
+            xoutSpatialData.setStreamName("spatialData")
+            xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+            #Set up mono camera resolution on left and right
+            monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+            monoLeft.setCamera("left")
+            monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+            monoRight.setCamera("right")
+
+            stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
+
+            #config roi
+            config = dai.SpatialLocationCalculatorConfigData()
+            config.depthThresholds.lowerThreshold = 100
+            config.depthThresholds.upperThreshold = 10000
+
+            config.roi = dai.Rect(self.topLeft, self.bottomRight)
+            
+            #Spatial location calculator
+            spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+            spatialLocationCalculator.initialConfig.addROI(config)
+
+            monoLeft.out.link(stereo.left)
+            monoRight.out.link(stereo.right)
+            spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
+            stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+            spatialLocationCalculator.out.link(xoutSpatialData.input)
+            xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+
+        #sync
+        sync = self.pipeline.create(dai.node.Sync)
 
         #Create LinkOut nodes
         xoutGrp = self.pipeline.create(dai.node.XLinkOut)
-
-        #Add for depth video sync
-        xoutDepth = self.pipeline.create(dai.node.XLinkOut)
-        xoutSpatialData = self.pipeline.create(dai.node.XLinkOut)
-        xinSpatialCalcConfig = self.pipeline.create(dai.node.XLinkIn)
+        xoutGrp.setStreamName("xout")
 
         #Add for IMU Xlink
         xlinkOut = self.pipeline.create(dai.node.XLinkOut)
-
-        #Set stream names for xout nodes
-        xoutGrp.setStreamName("xout")
-
-        #Add for depth video sync
-        xoutDepth.setStreamName("depth")
-        xoutSpatialData.setStreamName("spatialData")
-        xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
         xlinkOut.setStreamName("imu")
-
-        #Set up mono camera resolution on left and right
-        monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        monoLeft.setCamera("left")
-        monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        monoRight.setCamera("right")
-
-        stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
-
-        #config roi
-        config = dai.SpatialLocationCalculatorConfigData()
-        config.depthThresholds.lowerThreshold = 100
-        config.depthThresholds.upperThreshold = 10000
-        
-        config.roi = dai.Rect(self.topLeft, self.bottomRight)
+    
 
         #Enable IMU
         imu.enableIMUSensor(dai.IMUSensor.ROTATION_VECTOR, 120)
         imu.setBatchReportThreshold(1)
         imu.setMaxBatchReports(10)
-        imu.out.link(xlinkOut.input)
 
-        #Spatial location calculator
-        spatialLocationCalculator.inputConfig.setWaitForMessage(False)
-        spatialLocationCalculator.initialConfig.addROI(config)
+       
 
-        color.setCamera("color")
+        
 
         #setting up sync timeout
         sync.setSyncThreshold(timedelta(milliseconds=10))
+        sync.setSyncAttempts(-1)
 
-        #linking
-        monoLeft.out.link(stereo.left)
-        monoRight.out.link(stereo.right)
-        spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
-        stereo.depth.link(spatialLocationCalculator.inputDepth)
 
-        spatialLocationCalculator.out.link(xoutSpatialData.input)
-        xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
-        
-        #link color video and disparity to sync
-        stereo.disparity.link(sync.inputs["disparity"])
+        imu.out.link(sync.inputs["imu"])
         color.video.link(sync.inputs["video"])
 
         sync.out.link(xoutGrp.input)
-        self.disparityMultiplier = 255.0 / stereo.initialConfig.getMaxDisparity()
+
+        if self.depth_on:
+            stereo.depth.link(sync.inputs["disparity"])
+            self.disparityMultiplier = 255.0 / stereo.initialConfig.getMaxDisparity()
         
     def start(self):
-
         # Pipeline is defined, now we can connect to the device
         with dai.Device(self.pipeline) as device:
 
             init_state = True
-            queue = device.getOutputQueue("xout", 10, False)
-
-            depthQueue = device.getOutputQueue(name="depth", maxSize=2, blocking=False)
-            spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=2, blocking=False)
-            spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
-            imuQueue = device.getOutputQueue(name="imu", maxSize=2, blocking=False)
+            queue = device.getOutputQueue("xout", 3, True)
+            
             baseTs = None
+
+            if self.depth_on:
+                depthQueue = device.getOutputQueue(name="depth", maxSize=2, blocking=False)
+                spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=2, blocking=False)
+                spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
 
             while True:
                 #get RGB data
                 msgGrp = queue.get()
 
                 #get IMU data
-                imuData = imuQueue.get()
-                imuPackets = imuData.packets
-
-                #Get depth data
-                inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
-                depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
-                depth_downscaled = depthFrame[::4]
-
-                if np.all(depth_downscaled == 0):
-                    min_depth = 0  # Set a default minimum depth value when all elements are zero
-                else:
-                    min_depth = np.percentile(depth_downscaled[depth_downscaled != 0], 1)
-
-                max_depth = np.percentile(depth_downscaled, 99)
-                depthFrameColor = np.interp(depthFrame, (min_depth, max_depth), (0, 255)).astype(np.uint8)
-                depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
-
-                spatialData = spatialCalcQueue.get().getSpatialLocations()
-                for depthData in spatialData:
-                    roi = depthData.config.roi
-                    roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
-                    
-                    #transform z --> x, x --> y, y --> -z to standard TF
-                    self.coordinates_3d = [int(depthData.spatialCoordinates.z),int(depthData.spatialCoordinates.x) ,-int(depthData.spatialCoordinates.y)]    
+                imuData = msgGrp['imu']
+                colorData = msgGrp['video']
 
                 #unpack RGB data
-                for name, msg in msgGrp:
-                    frame_get = msg.getCvFrame()
+                
 
-                    if name == "disparity":
-                        self.frame["disparity"] = (frame_get * self.disparityMultiplier).astype(np.uint8)
-                        self.frame["disparity"]= cv2.applyColorMap(self.frame["disparity"], cv2.COLORMAP_JET)
+                if self.depth_on:
+                    #Get depth data
+                    inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
+                    depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
+                    depth_downscaled = depthFrame[::4]
+
+                    if np.all(depth_downscaled == 0):
+                        min_depth = 0  # Set a default minimum depth value when all elements are zero
                     else:
-                        self.frame["video"] = cv2.resize(frame_get, (960,540))[ 30:510, 160:800]
+                        min_depth = np.percentile(depth_downscaled[depth_downscaled != 0], 1)
+
+                    max_depth = np.percentile(depth_downscaled, 99)
+                    depthFrameColor = np.interp(depthFrame, (min_depth, max_depth), (0, 255)).astype(np.uint8)
+                    depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+                    spatialData = spatialCalcQueue.get().getSpatialLocations()
+                    for depthData in spatialData:
+                        roi = depthData.config.roi
+                        roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+                        
+                        #transform z --> x, x --> y, y --> -z to standard TF
+                        self.coordinates_3d = [int(depthData.spatialCoordinates.z),int(depthData.spatialCoordinates.x) ,-int(depthData.spatialCoordinates.y)]    
+
+
+
+                frame_get = colorData.getCvFrame()
+                self.frame["video"] = cv2.rotate(cv2.resize(frame_get, (640,360)),cv2.ROTATE_180)
+
+                if self.depth_on:
+                    self.frame["disparity"] = (frame_get * self.disparityMultiplier).astype(np.uint8)
+                    self.frame["disparity"]= cv2.applyColorMap(self.frame["disparity"], cv2.COLORMAP_JET)
 
                 #unpack IMU data
-                for imuPacket in imuPackets:
+                latestRotVec = imuData.packets[-1].rotationVector
 
-                    rVvalues = imuPacket.rotationVector
-                    rvTs = rVvalues.getTimestampDevice()
+               
 
-                    if baseTs is None:
-                        baseTs = rvTs
-                    rvTs = rvTs - baseTs
+                # set precision
+                # imuF = "{:.06f}"
+                # tsF  = "{:.03f}"
 
-                    # set precision
-                    # imuF = "{:.06f}"
-                    # tsF  = "{:.03f}"
-
-                    #i : yaw, j : roll, k : pitch
-                    self.cur_angle = CvHelper.euler_from_quaternion(rVvalues.i,rVvalues.j,rVvalues.k,rVvalues.real)
+                #i : yaw, j : roll, k : pitch
+                self.cur_angle = CvHelper.euler_from_quaternion(latestRotVec.i,latestRotVec.j,latestRotVec.k,latestRotVec.real)
                     
-                    if init_state:
-                        self.angles_default = self.cur_angle
-                        init_state = False
-                
-                config = dai.SpatialLocationCalculatorConfigData()    
-                config.roi = dai.Rect(self.topLeft, self.bottomRight)
-                config.calculationAlgorithm = self.calculationAlgorithm
-                cfg = dai.SpatialLocationCalculatorConfig()
-                cfg.addROI(config)
-                spatialCalcConfigInQueue.send(cfg)
+                if init_state:
+                    self.angles_default = self.cur_angle
+                    self.angles_min[2] = self.angles_default[2]
+                    init_state = False
+
+                # config = dai.SpatialLocationCalculatorConfigData()    
+                # config.roi = dai.Rect(self.topLeft, self.bottomRight)
+                # config.calculationAlgorithm = self.calculationAlgorithm
+                # cfg = dai.SpatialLocationCalculatorConfig()
+                # cfg.addROI(config)
+                # spatialCalcConfigInQueue.send(cfg)
 
     def get_frame(self):
         return self.frame
--- testt_no_depth.py
+++ testt_no_depth.py
@@ -14,7 +14,10 @@ from filterpy.kalman import ExtendedKalmanFilter as EKF
 from filterpy.common import Q_discrete_white_noise
 
 def main():
-    model = YOLO("./models/best.pt")
+
+    debug_mode = False
+
+    model = YOLO("./models/best.engine")
 
     enemy = target.Target()
     cam = CamDataCap.depth_camera()
@@ -33,8 +36,10 @@ def main():
 
     Global_xyz_filtered = [0,0,0]
 
-    target_yaw_record = np.empty(70)
-    target_pitch_record = np.empty(70)
+    target_yaw_record = np.zeros(1)
+    target_pitch_record = np.zeros(1)
+
+    yaw_record = np.zeros(1)
 
     thread2 = Thread(target = enemy.call_heartBeat,args=())
     thread2.start()
@@ -43,26 +48,45 @@ def main():
     thread1 = Thread(target = cam.start,args=())
     thread1.start()
     print("Camera Thread start")
-
+    detected = False
     time.sleep(4)
+
+    enemy.set_pitch_lower_limit(cam.angles_min[1])
+    enemy.ack_start(True)
+   
 
     while True:
         frame = cam.frame
+        #get current imu angle
+        
+        angle_rt= cam.cur_angle
+        cur_angle = - np.array(angle_rt) + np.array(cam.angles_default)
+        cur_angle[2] = CvHelper.wrap_angle(cur_angle[2])
+        yaw_record = np.append(yaw_record,cur_angle[2])
 
+        detected = False
         if not frame["video"] is None:
 
-            if frame["video"].shape == (480,640,3):
+            if frame["video"].shape == (360,640,3):
 
-                start_time = time.time()  # Record the start time
-                results = model(frame["video"],verbose=False)[0]
-                end_time = time.time() 
-                elapsed_time = end_time - start_time 
-                print('filter time: ', elapsed_time)
+                if debug_mode:
+                    start_time = time.time()  # Record the start time
+                    results = model.track(frame["video"],verbose=False,device="cuda",batch=16,persist=True,task='detect',conf=0.6)[0]
+                    end_time = time.time() 
+                    elapsed_time = end_time - start_time 
+                    print('video time: ', elapsed_time)
+                else:
+                    results = model.track(frame["video"],verbose=False,device="cuda",batch=16,persist=True,task="detect",conf=0.6)[0]
 
                 for result in results:
                     if result != None:
+                       
 
-                        enemy.is_detected(True)
+
+                        detected = True
+                        enemy.is_detected(detected)
+
+                        angle_record.append(cur_angle[2])
 
                         boxes_xy = result.boxes.xyxy[0]
 
@@ -72,85 +96,82 @@ def main():
                         # print(center_x, center_y)
                         # topLeft = dai.Point2f(((4*(center_x-7))/4056), ((((center_y-7)+30)*4+220)/3040))# (960,540))[ 30:510, 160:800]
                         # bottomRight = dai.Point2f(((4*(center_x+7))/4056),((((center_y+7)+30)*4+220)/3040))
-                        
 
                         frame["video"] = cv2.circle(frame["video"], (center_x,center_y), 10, (255, 0, 0) , 2)
-                        frame["disparity"] = cv2.circle(frame["disparity"], (int((((4*(center_x+160))/4056)*640)),int(400*(((center_y+30)*4+220)/3040))), 60, (255, 255, 255) , 2)
                         
-                        focal_length_in_pixel = 1280 * 4.81 / 11.043412
-                        focal_length_in_pixel_pitch =  1.8*960 * 4.81 / 11.043412
+                        focal_length_in_pixel = 1980*0.55* 4.81 / 11.043412
+                        focal_length_in_pixel_pitch =  1.5*1080 * 4.81 / 11.043412
                         
                         theta  = math.atan((center_x - 320)/focal_length_in_pixel)
-                        phi = math.atan((center_y - 240)/focal_length_in_pixel_pitch)
-                        
-                        theta_record.append(theta)
+                        phi = math.atan((center_y - 180)/focal_length_in_pixel_pitch)
+                        print(theta)
+                        print(cur_angle[2])
 
-                        # if abs(theta) >0.25:
-                        #     theta = theta * 2
-
-                        angle_rt= cam.cur_angle
-                        cur_angle = np.array(angle_rt) - np.array(cam.angles_default)
-
-                        cur_angle[2] = CvHelper.wrap_angle(-cur_angle[2])
-                        angle_record.append(cur_angle[2])
-
+                    
                         #roll pitch yaw
-                        # Yaw clockwise + 
+                        # Yaw clockwise +
                         # Pitch down +
                         #print("theta is: =",theta)
                         Global_xyz[2]= cur_angle[2]+theta
                         Global_xyz[1]= phi - cur_angle[1]
 
                         target_yaw_record = np.append(target_yaw_record,Global_xyz[2])
+                        yaw_record = np.append(yaw_record,Global_xyz[2])
+
                         target_pitch_record = np.append(target_pitch_record,Global_xyz[1])
 
-                        start_time = time.time()  # Record the start time
-                        target_pitch_record[-1], target_yaw_record[-1]= CvHelper.ellip_filter(target_pitch_record[-70:],target_yaw_record[-70:])
-                        end_time = time.time()  # Record the end time
-                        elapsed_time = end_time - start_time 
-                        print('filter time: ', elapsed_time)
-                        # target_yaw_window = target_yaw_record[-10:]
-                        # z = np.polyfit([0,1,2,3,4,5,6,7,8,9],target_yaw_window,3)
-                        # pred = z[0]*10 + z[1]
+                        # print(target_yaw_record[-1])
                         
+                        if debug_mode:
+                            start_time = time.time()  # Record the start time
+                            target_pitch_record[-1], target_yaw_record[-1]= CvHelper.ellip_filter(target_pitch_record,yaw_record)
+                            end_time = time.time()  # Record the end time
+                            elapsed_time = end_time - start_time 
+                            print('filter time: ', elapsed_time)
+                        else:
+                            target_pitch_record[-1], target_yaw_record[-1]= CvHelper.ellip_filter(target_pitch_record,yaw_record)
+                        
+                        theta_record.append(theta)
+                        location_record.append(Global_xyz[2])
+                        target_record.append(Global_xyz_filtered[2])
+
+
                         # if z[0] >5:
                         #     target_yaw_record[-1] *= 1.5
 
-                        Global_xyz_filtered[2] = target_yaw_record[-1]-0.15
-                        # Global_xyz_filtered[2] = pred -0.1
-                        Global_xyz_filtered[1] = target_pitch_record[-1]-0.1
+                        Global_xyz_filtered[2] = CvHelper.wrap_angle(Global_xyz[2]+0.2)
+                        Global_xyz_filtered[1] = target_pitch_record[-1]-0.03
                         
-                       # Initialize the EKF
-                        ekf = EKF(dim_x=1, dim_z=2)
+                    #    # Initialize the EKF
+                    #     ekf = EKF(dim_x=1, dim_z=2)
 
-                        # Initial state
-                        ekf.x = np.array([0.0])  # Starting angle
-                        ekf.F = np.array([[1]])  # State transition matrix
-                        ekf.R = np.array([[0.1]])  # Measurement noise covariance
-                        ekf.Q = Q_discrete_white_noise(dim=2, dt=1, var=0.05)  # Process noise covariance
-                        ekf.P *= 1000  # Initial state covariance
+                    #     # Initial state
+                    #     ekf.x = np.array([0.0])  # Starting angle
+                    #     ekf.F = np.array([[1]])  # State transition matrix
+                    #     ekf.R = np.array([[0.1]])  # Measurement noise covariance
+                    #     ekf.Q = Q_discrete_white_noise(dim=2, dt=1, var=0.05)  # Process noise covariance
+                    #     ekf.P *= 1000  # Initial state covariance
 
-                        # Example measurements (angles from -pi to pi)
-                        measurements = target_yaw_record[-10:]   # Example angle measurements in radians
-                        dt = 1.0  # Time step
-
+                    #     # Example measurements (angles from -pi to pi)
+                    #     measurements = target_yaw_record[-10:]   # Example angle measurements in radians
+                    #     dt = 1.0  # Time step
 
                         Global_xyz_filtered = np.float32(Global_xyz_filtered)
                         enemy.set_target_angle(Global_xyz_filtered)
 
                         #print global location for debug
-                        location_record.append(Global_xyz[2])
-                        target_record.append(Global_xyz_filtered[2])
+
                         
                         last = Global_xyz
                         
                         break
                     else:
-                        Global_xyz_filtered[2]+= 0.1
                         enemy.is_detected(False)
+                
+                if not detected:
+                    enemy.is_detected(False)
 
         cv2.imshow("video", frame["video"])
-        cv2.imshow("depth", frame["disparity"])
 
         if cv2.waitKey(1) == ord('q'):
             break
