3a03757d13248b7a80b0e1c4b8dd5c3fa37fb3a3
Bill <Onbilllin@gmail.com>
2024-06-13 04:13:03 -0400

    add enemy differentialize

--- target.py
+++ target.py
@@ -1,5 +1,7 @@
 import CvCmdApi
 import time
+import cv2
+import numpy as np
 
 class Target:
 
@@ -8,6 +10,7 @@ class Target:
         self.enemy_detected = False
         self.Global_xyz_filtered = [0, 0, 0]
         self.CvCmder = CvCmdApi.CvCmdHandler('/dev/ttyTHS1')
+        self.color = self.CvCmder.CvCmd_GetTeamColor()
     
     def set_target_angle(self, target_angle):
         self.target_angle = target_angle
@@ -20,6 +23,34 @@ class Target:
 
     def get_enemy_detected(self):
         return self.enemy_detected
+
+    def is_enemy(self,crop):
+        crop=cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
+
+        # Define color ranges for red and blue
+        lower_red = np.array([0, 120, 70])
+        upper_red = np.array([10, 255, 255])
+
+        lower_blue = np.array([100, 120, 70])
+        upper_blue = np.array([130, 255, 255])
+
+        # Create masks for each color range
+        red_mask = cv2.inRange(crop, lower_red, upper_red)
+        blue_mask = cv2.inRange(crop, lower_blue, upper_blue)
+
+        # Count the number of pixels for each color
+        red_pixels = cv2.countNonZero(red_mask)
+        blue_pixels = cv2.countNonZero(blue_mask)
+
+        # Determine the dominant color
+        if red_pixels > blue_pixels:
+            Target = 1   # Red
+        elif blue_pixels > red_pixels:
+            Target = 0   # Blue
+
+        if Target != self.color:
+            return True
+    
 
     # Heartbeat from CV to Control
     def call_heartBeat(self):
